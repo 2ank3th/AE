72fb00349840e6f7cf7d933def7bb3c4bb2bca82
Sanketh Purwar <sankethpurwar@172-16-201-216.DYNAPOOL.NYU.EDU>
2017-03-03 18:10:24 -0500

    iterations working

--- ladder_impl.py
+++ ladder_impl.py
@@ -7,7 +7,7 @@ import argparse
 import math
 import os
 import csv
-import pickle
+import cPickle as pickle
 
 import numpy as np
 import torch.nn as nn
@@ -29,7 +29,7 @@ parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=1, metavar='N',
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
@@ -83,7 +83,7 @@ join = lambda l, u: torch.cat([l, u], 0)
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
-print(trainset_labeled.train_data.size())
+#print(trainset_unlabeled.train_labels.size())
 train_loader = torch.utils.data.DataLoader(trainset_labeled , batch_size=64, shuffle=True, **kwargs)
 unlabeled_train_loader = torch.utils.data.DataLoader( trainset_unlabeled, batch_size=64, shuffle=True, **kwargs)
 
@@ -110,7 +110,6 @@ class VAE(nn.Module):
             return None, h
 
     def g_gauss(self,z_c, u, size):
-        print("sizze" + str(size))
         "gaussian denoising function proposed in the original paper"
         wi = lambda inits, name: Variable( torch.ones(1,size).expand(u.size()[0],size), requires_grad = True) #TODO possible reason of bad accuracy
         # wi = lambda inits, name: tf.Variable(inits * tf.ones([size]), name=name)
@@ -126,8 +125,7 @@ class VAE(nn.Module):
         a9 = wi(0., 'a9')
         a10 = wi(0., 'a10')
 
-        print("u" + str(u.size()))
-        print("a2" + str(a2.size()))
+
         temp1 = a2 * u
         temp2 = temp1 + a3
         temp3 = a4 * u
@@ -150,7 +148,7 @@ class VAE(nn.Module):
 
         for l in xrange(1,L+1):
 
-            print("Layer " + str(l)+ ": "+ str(layer_sizes[l - 1])+ " -> "+ str(layer_sizes[l]))
+            #print("Layer " + str(l)+ ": "+ str(layer_sizes[l - 1])+ " -> "+ str(layer_sizes[l]))
 
             d['h'][l - 1] = h
 
@@ -161,9 +159,6 @@ class VAE(nn.Module):
 
             #TODO why only for unlabbeled if not labeled:
             m, v = torch.mean(z_pre_u, 1), torch.var(z_pre_u, 1)
-
-            print("Mean : "+ str(m))
-            print("Var : " + str(v))
 
             d['m'][l], d['v'][l] = m, v  # save mean and variance of unlabeled examples for decoding
 
@@ -198,8 +193,7 @@ class VAE(nn.Module):
         d_cost = Variable(torch.FloatTensor(L + 1), requires_grad=False) # to store the denoising cost of all layers
 
         for l in range(L, 0, -1): #TODO: last layer not run
-            print ("Layer ", l, ": ", layer_sizes[l + 1] if l + 1 < len(layer_sizes) else None, " -> ", layer_sizes[
-                l], ", denoising cost: ", denoising_cost[l])
+            #print ("Layer ", l, ": ", layer_sizes[l + 1] if l + 1 < len(layer_sizes) else None, " -> ", layer_sizes[l], ", denoising cost: ", denoising_cost[l])
 
             z, z_c = d_clean['z'][l], d_corr['z'][l]
 
@@ -214,7 +208,6 @@ class VAE(nn.Module):
             batch_norm = torch.nn.BatchNorm1d(u.size()[1])
 
             u = batch_norm(u)
-            print(m)
 
             z_est[l] = self.g_gauss(z_c, u, layer_sizes[l])
             z_est_bn = (z_est[l] - m.expand_as(z_est[l])) / v.expand_as(z_est[l])
@@ -225,7 +218,7 @@ class VAE(nn.Module):
 
         return d_cost
 
-    def forward(self, (x,labeled)):
+    def forward(self, (x,target,labeled)):
 
         h_corr,d_corr = self.encoder(x,0.3,labeled) #TODO: add noise
         h_clean,d_clean = self.encoder(x,0,labeled)
@@ -238,18 +231,15 @@ class VAE(nn.Module):
         #for ele in d_cost:
         #    u_cost += ele
         # u_cost = tf.add_n(d_cost)
-        print('xxxxxxxxx')
-        print("d_cost: " + str(d_cost))
+        #print('xxxxxxxxx')
+        #print("d_cost: " + str(d_cost))
         u_cost = torch.sum(d_cost)
-        print('xxxxxxxxx')
-        print("u_cost: " + str(u_cost))
-        print("u_cost: " + str(u_cost.size()))
+        #print('xxxxxxxxx')
+        #print("u_cost: " + str(u_cost.size()))
 
-        #y_N = labeled(y_c)
-        #cost = -torch.mean(tf.torch.sum(outputs * torch.log(y_N), 1))  # supervised cost
-        # cost = -tf.reduce_mean(tf.reduce_sum(outputs*tf.log(y_N), 1))  # supervised cost
-        #loss = cost + u_cost  # total cost
-
+        if labeled:
+            s_cost = torch.nn.functional.cross_entropy(h_clean,target)  # supervised cost
+            return u_cost + s_cost
 
         return u_cost
 
@@ -262,25 +252,33 @@ def train(epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = Variable(data), Variable(target)
+
         data = data.view(-1,784) #TODO possible reason of bad accuracy
         optimizer.zero_grad()
-        loss = model((data, True))
+        loss = model((data, target,True))
         loss.backward()
         optimizer.step()
-
-        for batch_idx, (data, target) in enumerate(unlabeled_train_loader):
-            data, target = Variable(data), Variable(target)
-            data = data.view(-1, 784)  # TODO possible reason of bad accuracy
-            optimizer.zero_grad()
-            loss = model((data, False))
-            loss.backward()
-            optimizer.step()
-
 
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                        100. * batch_idx / len(train_loader), loss.data[0]))
+
+    for batch_idx, (data, target) in enumerate(unlabeled_train_loader):
+        data, target = Variable(data), Variable(target)
+        data = data.view(-1, 784)  # TODO possible reason of bad accuracy
+        optimizer.zero_grad()
+        loss = model((data, target,False))
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.data[0]))
+
+
+
 
 
 
