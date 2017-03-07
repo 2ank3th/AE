3156e35283395ac989d826e428f9becba4019411
Sam Ho <Sam@172-17-84-69.DYNAPOOL.NYU.EDU>
2017-03-06 21:39:47 -0500

    added accuracy data

--- ladder_impl_sam.py
+++ ladder_impl_sam.py
@@ -352,6 +352,26 @@ def train(epoch):
                         epoch, batch_idx * len(data), len(train_loader.dataset),
                                100. * batch_idx / len(train_loader), loss.data[0]))
 
+def test(epoch, valid_loader):
+
+    model.eval()
+    test_loss = 0
+    correct = 0
+
+    for data, target in valid_loader:
+        data, target = Variable(data, volatile=True), Variable(target)
+        data = data.view(-1, 784)
+        output,loss = model(data,target, True, True)
+        test_loss += F.nll_loss(output, target).data[0]
+        pred = output.data.max(1)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data).cpu().sum()
+
+    test_loss /= len(valid_loader) # loss function already averages over batch size
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(valid_loader.dataset),
+        100. * correct / len(valid_loader.dataset)))
+
+
 for epoch in range(1, args.epochs + 1):
     train(epoch)
-    #test(epoch, valid_loader)
+    test(epoch, valid_loader)
